347e046dddefdbba69cc4464a22a85088f370090
Andre Castillo <andrecastillo@gmail.com>
2021-01-14 20:47:05 -0500

    i've got the navbar all bootstrapped now and have active links to show what page your on, not like you wouldn't now, but whatever, it looks better.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,9 +7,9 @@
 
 <!-- navigation -->
 <ul class="navbar-nav mr-auto">
-    <a class="nav-item nav-link" style="font-size: 1.5rem;" href="{{ route('principles.home') }}">Principles</a>
-    <a class="nav-item nav-link" style="font-size: 1.5rem;" href="{{ route('missions.home') }}">Missions</a>
-    <a class="nav-item nav-link" style="font-size: 1.5rem;" href="{{ route('goals.home') }}">Goals</a>
-    <a class="nav-item nav-link" style="font-size: 1.5rem;" href="{{ route('tasks.index') }}">Tasks</a>
+    <a class="nav-item nav-link {{ request()->is("principles") ? 'active' : '' }}" style="font-size: 1.5rem;" href="{{ route('principles.home') }}">Principles</a>
+    <a class="nav-item nav-link {{ request()->is("missions") ? 'active' : '' }}" style="font-size: 1.5rem;" href="{{ route('missions.home') }}">Missions</a>
+    <a class="nav-item nav-link {{ request()->is("goals") ? 'active' : '' }}" style="font-size: 1.5rem;" href="{{ route('goals.home') }}">Goals</a>
+    <a class="nav-item nav-link {{ request()->is("tasks") ? 'active' : '' }}" style="font-size: 1.5rem;" href="{{ route('tasks.index') }}">Tasks</a>
 </ul>
 @endauth
