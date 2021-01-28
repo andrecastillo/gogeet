93759195824642afd5e430e9ae54cf029a15e4af
Andre Castillo <andrecastillo@gmail.com>
2021-01-27 23:12:25 -0500

    little fix, no longer need to get originals, so got rid of that.

--- resources/js/components/PrincipleDetails.vue
+++ resources/js/components/PrincipleDetails.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="container">
 
-    <input class="form-control form-control-lg mt-2 w-100" @focusin="getOriginalName" @focusout="editPrinciple" v-model="principle.name">
+    <input class="form-control form-control-lg mt-2 w-100" @focusout="editPrinciple" v-model="principle.name">
 
     <div class="ml-3">
         <div class="row">
             <label class="col-2">Description: </label>
-            <textarea class="form-control" @focusin="getOriginalDescription" @focusout="editPrinciple" placeholder="-----" v-model="principle.description"></textarea>
+            <textarea class="form-control" @focusout="editPrinciple" placeholder="-----" v-model="principle.description"></textarea>
         </div>
 
         <div class="blockquote-footer">
@@ -48,37 +48,31 @@ export default {
             });
         },
 
-        getOriginalName: function ()
-        {
-            this.original_name = this.principle.name;
-        },
-
-        getOriginalDescription: function()
-        {
-            this.original_description = this.principle.description;
-        },
-
         editPrinciple: function()
         {
-            axios.patch('/principle/' + this.principle.id, {
-                name: this.principle.name,
-                description: this.principle.description,
-            })
-            .then(response => {
-                // update last updated text
-                this.principle.updated_at = response.data.updated_at;
+            if (this.original_name !== this.principle.name || this.original_description !== this.principle.description) {
+                axios.patch('/principle/' + this.principle.id, {
+                    name: this.principle.name,
+                    description: this.principle.description,
+                })
+                .then(response => {
+                    // update last updated text
+                    this.principle.updated_at = response.data.updated_at;
 
-                // now update the value in the sibling component if it was name not description
-            })
-            .catch(error => {
-                this.errors = [];
-                if (error.response.data.errors.name) {
-                    this.errors.push(error.response.data.errors.name[0]);
-                }
-                if (error.response.data.errors.description) {
-                    this.errors.push(error.response.data.errors.description[0]);
-                }
-            });
+                    // now update the value in the sibling component if it was name not description
+                })
+                .catch(error => {
+                    this.errors = [];
+                    if (error.response.data.errors.name) {
+                        this.errors.push(error.response.data.errors.name[0]);
+                    }
+                    if (error.response.data.errors.description) {
+                        this.errors.push(error.response.data.errors.description[0]);
+                    }
+                });
+            } else {
+                console.log('no change so, don');
+            }
         },
 
         _formatDateTime(date, format = 'MM/DD/YYYY hh:mm A') {
