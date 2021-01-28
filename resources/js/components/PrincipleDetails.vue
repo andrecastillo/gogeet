27107ac1611a3f1f9d6ac7f3f4c455d6a0dbd726
Andre Castillo <andrecastillo@gmail.com>
2021-01-27 21:09:13 -0500

    wip

--- resources/js/components/PrincipleDetails.vue
+++ resources/js/components/PrincipleDetails.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="container">
 
-    <h4 class="mt-2">{{ principle.name }}</h4>
+    <h4 class="mt-2" contenteditable="true" @focusout="editName" id="name">{{ principle.name }}</h4>
 
     <div class="row">
         <div class="col-2">Description: </div>
-        <div class="col">{{ principle.description || "-----" }}</div>
+        <div class="col" contenteditable="true">{{ principle.description || "-----" }}</div>
     </div>
 
     <div class="blockquote-footer">
@@ -29,7 +29,8 @@ export default {
                 name: '',
                 updated_at: '',
                 user_id: '',
-            }
+            },
+            original_name: '',
         }
     },
     created() {
@@ -43,6 +44,30 @@ export default {
             .then(response => {
                 this.principle = response.data.principle;
             });
+        },
+
+        editName: function ()
+        {
+            this.original_name = this.principle.name;
+            this.new_name = 'tst';
+            console.log(this.$refs.name);
+            /*
+            axios.patch('/principle/' + this.update_principle.id, {
+                name: this.update_principle.name,
+            })
+            .then(response => {
+                $("#update_principle_model").modal("hide");
+            })
+            .catch(error => {
+                this.errors = [];
+                if (error.response.data.errors.name) {
+                    this.errors.push(error.response.data.errors.name[0]);
+                }
+                if (error.response.data.errors.description) {
+                    this.errors.push(error.response.data.errors.description[0]);
+                }
+            });
+            */
         },
 
         _formatDateTime(date, format = 'MM/DD/YYYY hh:mm A') {
